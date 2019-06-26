c9b56beef8a68fbe3b71ef4b04c80957a41328a0
Fumihiro Teshima <teshima.fu@gmail.com>
2019-06-26 10:16:13 +0900

    add ch02/ex04~06

---
+++ ch02/ex06/secondPerDay.py
@@ -0,0 +1,6 @@
+second = 1
+minutes = second * 60
+seconds_per_hour = minutes * 60
+seconds_per_day = seconds_per_hour * 24
+print(seconds_per_day / seconds_per_hour)
+print(seconds_per_day // seconds_per_hour)
